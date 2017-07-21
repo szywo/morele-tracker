79519d70dbbb97da1a07d35150adba7ec60e8605
szywo <szymon@wojdan.tk>
2017-07-21 13:26:16 +0200

    Major refactoring, external config and input file, debug switch

--- README.md
+++ README.md
@@ -1,40 +1,43 @@
 # morele-tracker
-A shell script to track prices and availability of morele.net products, intended to use with `cron`.
+A shell script to track prices and availability of morele.net products, at the beginning intended to use with `cron`.
 
 It works for now, but there is not any guarantee it will in the future, as it does not parse DOM tree. It only finds and parse some easily distinguishable lines of morele.net's product page code.
 
 The script was tested with dash (the Debian Almquist Shell) so it should work with any POSIX shell.
 
-#### !!! IMPORTANT !!!
-The products' urls are currently included in script code, there is no way to pass them from comand line (will consider it), file (1st TODO) or to find them by product id (morele.net website limitation)
-
-To include interesting product, you have to find its page on morele.net
-and copy product url to section:
-
-`!!! PUT HERE MORELE.NET PRODUCTS URLS !!!`
-
-using format:
-
-`parse_morele_page <morele_product_url>`
-
 ## Basic functionality implemented so far:
-`morele-tracker.sh`
+`morele-tracker.sh [-v]`
 
-`morele-tracker.sh print`
+`morele-tracker.sh [-v] print`
   - prints current prices and quantities
 
-`morele-tracker.sh log`
+`morele-tracker.sh [-v] log`
   - logs current prices and quantities to a file
 
-`morele-tracker.sh replay [filter]`
+`morele-tracker.sh [-v] replay [filter]`
   - prints archived prices and quantities, if optional filter string is given, script will print only lines containing this string (eg. to filter interesting product)
 
-calling script with anything else as first argument will print basic help
+Calling the script with anything else as first argument or with wrong (non-existent) switch will print basic help.
+
+### Important files
+`./morele-urls.list`
+- contains interesting product's urls; remember to leave the newline on a last line of the file
+
+`./morele-tracker.conf`
+- makes it possible to change defautl input and output files names (and paths) but only relative to the script's path
+
+`./log/morele-tracker.log`
+- stores data generated when using script's `log` command and is read by `replay` command
+
+`./.curl-cache.temp`
+- is temporary file that stores fetched pages until they are parsed. It is automaticaly deleted after the script's end.
 
 ## TODO list:
-  - [ ] 1. Read urls from separate file.
-  - [ ] 2. Refactor parse_morele_page() to return string and move printing/logging swith outside of the function.
-  - [ ] 3. Move timestamp generation to separate function.
-  - [ ] 4. Not enough already? Ok, I have a whole lot more ideas but don't have time to name them now, could say I have TODO to do.
+  - [x] 1. Read urls from separate file.
+  - [x] 2. Refactor ~~parse_morele_page()~~ parse_page() to return string and move recognition of printing/logging option outside of the function.
+  - [x] 3. Move timestamp generation to separate function.
+  - [ ] 4. Add more switches to point to input/output and config files.
+  - [ ] 5. Enable use of absolute IO/conf file paths.
+  - [ ] 6. Not enough already? Ok, I have a whole lot more ideas but don't have time to name them now, could say I have TODO to do.
   - ...
   - [ ] 777. Rewrite it in php, java or c/c++ to give it even more functionality or make morele.net hire me to write nice WEB API for them (;
--- morele-tracker.sh
+++ morele-tracker.sh
@@ -12,136 +12,290 @@
 #                                                                              #
 ################################################################################
 #                                                                              #
-#  !!! IMPORTANT !!!                                                           #
-#  Product urls are currently included in script code, there is no way to      #
-#  pass them from comand line (will consider it), file (1st TODO) or to        #
-#  find them by product id (morele.net website limitation)                     #
+#  Products' urls are read from a file named:                                  #
 #                                                                              #
-#  To include interesting product, you have to find its page on morele.net     #
-#  and copy product url to section:                                            #
+#     morele-urls.list                                                         #
 #                                                                              #
-#      !!! PUT HERE MORELE.NET PRODUCTS URLS !!!                               #
+#  located in the script's directory.                                          #
 #                                                                              #
 ################################################################################
 #                                                                              #
 #  Basic functionality implemented so far:                                     #
 #                                                                              #
-#  morele-tracker.sh                                                           #
-#  morele-tracker.sh print                                                     #
+#  morele-tracker.sh [-v]                                                      #
+#  morele-tracker.sh [-v] print                                                #
 #     - prints current prices and quantities                                   #
 #                                                                              #
-#  morele-tracker.sh log                                                       #
+#  morele-tracker.sh [-v] log                                                  #
 #     - logs current prices and quantities to a file                           #
 #                                                                              #
-#  morele-tracker.sh replay [filter]                                           #
+#  morele-tracker.sh [-v] replay [filter]                                      #
 #     - prints archived prices and quantities, if optional filter string       #
 #       is given, script will print only lines containing this string          #
 #       (eg. to filter interesting product)                                    #
 #                                                                              #
-#  calling script with anything else as first argument will print basic help   #
+#  Switches:                                                                   #
+#    -v   Makes the script more verbose/talkative during the operation.        #
+#         All verbose messages are redirected to stderr.                       #
+#                                                                              #
+#  Calling the script with anything else as first argument or with wrong       #
+#  (non-existent) switch will print basic help.                                #
 #                                                                              #
 ################################################################################
 
 
 ################################################################################
 #                                                                              #
-#  log and cache file names                                                    #
+#  Default file names and paths                                                #
 #                                                                              #
 ################################################################################
-cachefile=".curl-cache"
-logfile="price-tracker-log"
-logdir="log"
+
+# Default debug level is 0 (no debugging) to change it use config file
+# or better -v switch
+DEFAULT_DEBUG_LEVEL=0
+
+# Get absolute script path
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+# For future use
+DEFAULT_CONFIG_FILE_NAME="morele-tracker.conf"
+
+# Input file with list of urls to check
+DEFAULT_INPUT_FILE_NAME="morele-urls.list"
+
+# Main data file name and its sub dir
+DEFAULT_LOG_FILE_NAME="price-tracker.log"
+DEFAULT_LOG_FILE_DIR="log"
+
+# Temporary file where fetched pages are stored for processing
+DEFAULT_CACHE_FILE_NAME=".curl-cache.temp"
+
 
 ################################################################################
 #                                                                              #
-#  setup paths for cache and log files to point to script's dir                #
+#  Check, read and parse config file values (future use)                       #
 #                                                                              #
 ################################################################################
-path=`echo $0 | sed 's/\/[^\/]*$//'`
-cachepath="$path/$cachefile"
-logpath="$path/$logdir/$logfile"
 
-command=${1}
-option=${2}
+# Check for config file and read it
+if [ -r "$SCRIPT_PATH/${CONFIG_FILE_NAME:=$DEFAULT_CONFIG_FILE_NAME}" ]; then
+  . "$SCRIPT_PATH/$CONFIG_FILE_NAME"
+fi
+
+# Parse config file values
+# Debug level
+DEBUG=${DEBUG_LEVEL:=$DEFAULT_DEBUG_LEVEL}
+
+# Set absolute input file path and name
+INPUT_FILE="$SCRIPT_PATH/${INPUT_FILE_NAME:=$DEFAULT_INPUT_FILE_NAME}"
+# Set absolute log dir path
+LOG_PATH="$SCRIPT_PATH/${LOG_DIR_NAME:=$DEFAULT_LOG_FILE_DIR}"
+# Set absolute log file path and name
+LOG_FILE="$LOG_PATH/${LOG_FILE_NAME:=$DEFAULT_LOG_FILE_NAME}"
+# Set absolute cache file path and name
+CACHE_FILE="$SCRIPT_PATH/${CACHE_FILE_NAME:=$DEFAULT_CACHE_FILE_NAME}"
 
 ################################################################################
 #                                                                              #
-#  function to retrive essential data from given morele product url            #
+#  Parse the switches                                                          #
 #                                                                              #
 ################################################################################
-parse_morele_page () {
-    curl -s $1 > $cachepath
-    date=`date +"%Y-%m-%d %H:%M%z"`
-    price=`cat $cachepath | grep 'id="product_price_brutto"' | sed 's/^.*content="\([0-9.]*\)".*/\1/'`
-    name=`cat $cachepath | grep -m 1 '<title>' | sed 's/^<title>\(.*\)\sw\sMorele\.net<\/title>/\1/'`
-    avail=`cat $cachepath | grep 'Dostępność: <b>' | sed 's/^Dostępność: <b>\(.*\)\sszt\.\s\?\([^<]*\)<\/b>/\1/'`
-    if [ -z "$avail" ]; then
-        avail=0
+while getopts v OPTION
+do
+  case $OPTION in
+    v)
+        DEBUG=1
+        ;;
+    \?)
+        OPT_COMMAND="error"
+        ;;
+  esac
+done
+
+shift `expr $OPTIND - 1`
+
+################################################################################
+#                                                                              #
+#  Parse the commands (options)                                                #
+#                                                                              #
+################################################################################
+# What we have to do
+[ "$OPT_COMMAND"!="error" ] && OPT_COMMAND=$1
+[ -z "$OPT_COMMAND" ] && OPT_COMMAND="print"
+
+# Option to filter replay output
+OPT_FILTER=$2
+
+
+################################################################################
+#                                                                              #
+#  Functions                                                                   #
+#                                                                              #
+################################################################################
+
+# Fetch a page to parse
+#
+# usage:
+#    fetch_page $URL_TO_FETCH $CACHE_FILE
+#
+fetch_page () {
+    if [ -z "$1" -o -z "$2" ]; then
+        return 1
     fi
-    case $command in
-        "" | "print")
-            echo [ $date ][ $price ][ $avail ][ $name ]
-            ;;
-        "log")
-            echo [ $date ][ $price ][ $avail ][ $name ] >> $logpath
-            echo -n "."
-            ;;
-    esac
-    rm $cachepath
+    curl -sfo $2 $1
+    if [ $? -ne 0 ]; then
+        return 2
+    fi
+    return 0
 }
 
+# Parse the fetched page
+#
+# usage:
+#    parse_page $CACHE_FILE
+#
+parse_page () {
+    FILE=$1
+    if [ ! -r "$1" ]; then
+        return 1
+    fi
+
+    NAME=$(cat $FILE | grep -m 1 '<title>' | sed 's/^<title>\(.*\)\sw\sMorele\.net<\/title>/\1/')
+    PRICE=$(cat $FILE | grep 'id="product_price_brutto"' | sed 's/^.*content="\([0-9.]*\)".*/\1/')
+    if [ -z "$NAME" -o -z "$PRICE" ]; then
+        return 2
+    fi
+
+    QTY=$(cat $FILE | grep 'Dostępność: <b>' | sed 's/^Dostępność: <b>\(.*\)\sszt\.\s\?\([^<]*\)<\/b>/\1/')
+    if [ -z "$QTY" ]; then
+        QTY=0
+    fi
+
+    printf "[ %8s ][ %4s ][ %s ]" "$PRICE" "$QTY" "$NAME"
+    return 0
+}
+
+# Generate a timestamp
+get_timestamp () {
+    printf "[ %s ]" "$(date +"%Y-%m-%d %H:%M%z")"
+    return 0
+}
+
+# Delete any remaining temporary files
+cleanup () {
+    if [ -e $CACHE_FILE ]; then
+        rm $CACHE_FILE
+    fi
+}
 
 ################################################################################
 #                                                                              #
-#  main script                                                                 #
+#  Main script                                                                 #
 #                                                                              #
 ################################################################################
-case $command in
+[ $DEBUG -ge 1 ] && echo "DEBUG: Executing choosen command: $OPT_COMMAND" >&2
+
+case $OPT_COMMAND in
     "log" | "print" | "")
-        if [ "$command" = "log" ]; then
-            if [ ! -d "$path/$logdir" ]; then
-                mkdir "$path/$logdir"
+        if [ ! -r "$INPUT_FILE" ]; then
+            echo "ERROR: Can not read $INPUT_FILE" >&2
+            exit 1
+        fi
+
+        [ $DEBUG -ge 1 ] && echo "DEBUG: Set the input file: $INPUT_FILE" >&2
+
+        if [ "$OPT_COMMAND" = "log" -a $DEBUG -eq 0 ]; then
+          printf "Processing"
+        fi
+
+        TIMESTAMP=$(get_timestamp)
+
+        [ $DEBUG -ge 1 ] && echo "DEBUG: Get a timestamp: $TIMESTAMP" >&2
+
+        [ $DEBUG -ge 1 ] && echo "DEBUG: Enter read loop:" >&2
+
+        while read URL ; do
+
+            [ $DEBUG -ge 1 ] && echo "DEBUG: Get the url: $URL ..." >&2
+
+            fetch_page "$URL" "$CACHE_FILE"
+            CMD_RESULT=$?
+
+            [ $DEBUG -ge 1 ] && echo "DEBUG:  ... check fetching result: $CMD_RESULT" >&2
+
+            if [ $CMD_RESULT -ne 0 ]; then
+                [ $CMD_RESULT -eq 1 ] && echo "Warning: wrong page_fetch call: $URL $CACHE_FILE" >&2
+                [ $CMD_RESULT -eq 2 ] && echo "Warning: \`curl\` could not fetch page $URL" >&2
+                continue
             fi
-            echo -n "Processing"
-        fi
-        ################################################################################
-        #                                                                              #
-        #      !!! PUT HERE MORELE.NET PRODUCTS URLS !!!                               #
-        #                                                                              #
-        ################################################################################
-        #                                                                              #
-        #  use format: parse_morele_page <morele_product_url>                           #
-        #                                                                              #
-        ################################################################################
-        # some sample urls (my recent wish fors)
-        parse_morele_page "https://www.morele.net/procesor-serwerowy-intel-xeon-e3-1231v3-3-4-ghz-lga1150-box-bx80646e31231v3-640233/"
-        parse_morele_page "https://www.morele.net/procesor-serwerowy-intel-xeon-e3-1241v3-3-5-ghz-lga1150-box-bx80646e31241v3-640234/"
-        parse_morele_page "https://www.morele.net/procesor-serwerowy-intel-xeon-e3-1270-v3-8m-cache-3-50-ghz-box-bx80646e31270v3-619018/"
-        parse_morele_page "https://www.morele.net/procesor-serwerowy-intel-xeon-e3-1271-v3-8m-cache-3-60-ghz-bx80646e31271v3-641043/"
-        parse_morele_page "https://www.morele.net/karta-graficzna-evga-geforce-gtx-1060-gaming-acx-2-0-3gb-gddr5-192-bit-hdmi-dvi-3xdp-box-03g-p4-6160-kr-1004503/"
-        parse_morele_page "https://www.morele.net/karta-graficzna-evga-geforce-gtx-1060-gaming-6gb-gddr5-192-bit-hdmi-dvi-3x-dp-box-06g-p4-6161-kr-1009506/"
-        parse_morele_page "https://www.morele.net/karta-graficzna-evga-geforce-gtx-1060-sc-gaming-6gb-gddr5-192-bit-dvi-d-3xdp-hdmi-box-06g-p4-6163-kr-1049651/"
-        parse_morele_page "https://www.morele.net/karta-graficzna-evga-geforce-gtx-1060-ftw-gaming-6gb-gddr5-192-bit-hdmi-dvi-d-3xdp-box-06g-p4-6268-kr-1114675/"
-        parse_morele_page "https://www.morele.net/drukarka-laserowa-hewlett-packard-laserjet-pro-400-m402dne-c5j91a-b19-1053579/"
-        parse_morele_page "https://www.morele.net/drukarka-laserowa-hewlett-packard-laserjet-pro-400-m402dn-c5f94a-775009/"
-        parse_morele_page "https://www.morele.net/drukarka-laserowa-hewlett-packard-hp-laserjet-pro-m402dw-c5f95a-936791/"
-        if [ "$command" = "log" ]; then
-            echo "done"
-        fi
-        ;;
-    "replay")
-        if [ -e $logpath ]; then
-            if [ -n "$option" ]; then
-                cat $logpath | grep $option
+
+            [ $DEBUG -eq 1 -a -r $CACHE_FILE ] && echo "DEBUG:  ... and store in $CACHE_FILE" >&2
+
+            [ $DEBUG -ge 1 ] && echo "DEBUG: Parse $CACHE_FILE ..." >&2
+
+            PARSED_RESULT=$(parse_page "$CACHE_FILE")
+            CMD_RESULT=$?
+
+            [ $DEBUG -ge 1 ] && echo "DEBUG:  ... parse_page status $CMD_RESULT returned value $PARSED_RESULT" >&2
+
+            if [ $CMD_RESULT -ne 0 ]; then
+                [ $CMD_RESULT -eq 1 ] && echo "Warning: can not read $CACHE_FILE of $URL" >&2
+                [ $CMD_RESULT -eq 2 ] && echo "Warning: could not parse $CACHE_FILE of $URL" >&2
+                continue
+            fi
+
+            if [ "$OPT_COMMAND" = "log" ]; then
+                if [ ! -d "$LOG_PATH" ]; then
+                    mkdir "$LOG_PATH"
+                    if [ $? -ne 0 ]; then
+                        echo "Error: could not create log dir: $LOG_PATH" >&2
+                        cleanup
+                        exit 2
+                    fi
+                fi
+                printf "%s%s\n" "$TIMESTAMP" "$PARSED_RESULT" >> $LOG_FILE
+                [ $DEBUG -eq 0 ] && printf "."
             else
-                cat $logpath
+                printf "%s%s\n" "$TIMESTAMP" "$PARSED_RESULT"
             fi
-        else
-            echo "Error, no log file found: $logpath"
+
+
+        done < $INPUT_FILE
+
+        [ $DEBUG -ge 1 ] && echo "DEBUG: Exit read loop." >&2
+
+        if [ "$OPT_COMMAND" = "log" -a $DEBUG -eq 0 ]; then
+            printf "done\n"
         fi
+
+        [ $DEBUG -ge 1 ] && echo -n "DEBUG: Cleanup ..." >&2
+
+        cleanup
+
+        [ $DEBUG -ge 1 ] && echo " done." >&2
+
         ;;
-    *)
-        echo "Usage: morele-tracker.sh print|log|replay [option]"
-        exit 0
+
+    "replay")
+        if [ ! -r $LOG_FILE ]; then
+            echo "Error, no log file found: $LOG_FILE" >&2
+            exit 3
+        fi
+
+        if [ -n "$OPT_FILTER" ]; then
+            cat $LOG_FILE | grep $OPT_FILTER
+        else
+            cat $LOG_FILE
+        fi
+
+        ;;
+
+    "error" | *)
+        echo "Usage: morele-tracker.sh [-v] print|log|replay [filter_string]" >&2
+        exit 1
         ;;
 esac
+
+[ $DEBUG -ge 1 ] && echo "DEBUG: Exit." >&2
+exit 0
