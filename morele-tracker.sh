8504960d67fab448311f513661b0de338c75017a
szywo <szymon@wojdan.tk>
2018-05-27 10:15:09 +0200

    Adjusted regexp for extracting quantity

--- morele-tracker.sh
+++ morele-tracker.sh
@@ -167,7 +167,7 @@ parse_page () {
         return 2
     fi
 
-    QTY=`cat $FILE | grep ' szt.</b>' | sed 's/\(tylko\s\)\?<b>\([0-9]*\)\sszt\.<\/b>/\2/'`
+    QTY=`cat $FILE | grep ' szt.</b>' | sed 's/^\(tylko\s\)\?<b>\([0-9]*\)\sszt\.<\/b>$/\2/'`
     if [ -z "$QTY" ]; then
         QTY=0
     fi
